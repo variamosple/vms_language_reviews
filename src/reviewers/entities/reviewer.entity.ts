0eb514fea164b8b995fdd151169ea288649f7971
Julian Alexander Murillo <julian.alexander.murillo@gmail.com>
2023-07-07 08:11:44 -0500

    Updating connections

--- src/reviewers/entities/reviewer.entity.ts
+++ src/reviewers/entities/reviewer.entity.ts
@@ -1,12 +1,12 @@
-import { LanguageReview } from "src/languagereviews/entities/languagereview.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { LanguageReview } from 'src/languagereviews/entities/languagereview.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Reviewer {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column('text')
   email: string;
 
   @ManyToOne(() => LanguageReview, (languageReview) => languageReview.id)
